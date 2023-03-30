f4f94c1536af61edd3d78cd5ddd6bc56e530ec45
Almazbek Temirkanov <atemirkanov@beeline.kg>
2023-03-30 10:19:28 +0600

    add ci file

--- src/main/java/com/almaz/bulbal/security/config/GoogleSecurityConfig.java
+++ src/main/java/com/almaz/bulbal/security/config/GoogleSecurityConfig.java
@@ -1,13 +1,8 @@
 package com.almaz.bulbal.security.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
 
 //@EnableWebSecurity
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
